d48047bc90c354533125b0c3573582c1466ddef6
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-11 23:59:12 +0100

    Create gabarit.php

---
+++ gabarit.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title><?= $titre ?></title>
+    </head>
+    <body>
+        <div id="global">
+            <header>
+                <a href="index.php"><h1 id="titre">CarLocation</h1></a>
+                <p>WELCOME TO CarLocation.</p>
+            </header>
+            <div id="contenu">
+                <?= $contenu ?>
+            </div> <!-- #contenu -->
+            <footer id="pied">
+                Application réalisé par DAHCHAR MOHAMMED SAID & IMANE MALAWI & MOHAMMED AMINE EL HADDAJI & INSAF SALMOUN &copy 2020.
+            </footer>
+        </div> <!-- #global -->
+    </body>
+</html>
