ab8642fe3b2f0e91785d252b2f0f65b5bb7e7514
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 03:58:31 +0100

    Create ControleurAccueil.php

---
+++ Controleur/ControleurAccueil.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'Framework/Controleur.php';
+require_once 'Modele/Voiture.php';
+
+class ControleurAccueil extends Controleur {
+
+    private $voiture;
+
+    public function __construct() {
+        $this->voiture = new Voiture();
+    }
+
+    // Affiche la liste de tous les voitures
+    public function index() {
+        voitures = $this->voiture->getVoitures();
+        $this->genererVue(array('voitures' => voitures));
+    }
+
+}
