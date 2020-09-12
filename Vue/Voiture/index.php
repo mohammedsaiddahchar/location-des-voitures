a926dd8d558efbea8fd741b98dcf5548cb49b6ad
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 04:27:00 +0100

    Create index.php

---
+++ Vue/Voiture/index.php
@@ -0,0 +1,35 @@
+<?php $this->titre = "CarLocation - " . $this->nettoyer($voiture['id']); ?>
+
+
+<article>
+    <header>
+        <h1 class="titre"><?= $this->nettoyer($voiture['marque']) ?></h1>
+    </header>
+    <p><?= $this->nettoyer($voiture['contenu']) ?></p>
+</article>
+<hr />
+<article>
+        <header>
+            <a href="<?= "voiture/index/" . $this->nettoyer($voiture['id']) ?>">
+                <h1 class="voiture"><?= $this->nettoyer($voiture['marque']) ?></h1>
+            </a>
+        </header>
+        <p>
+            <table>
+        <tr><img src="<?= $this->nettoyer($voiture['image'])  ?>"></tr>
+    <tr><td>marque</td><td><?= $this->nettoyer($voiture['marque']) ?></td></tr>
+    <tr><td>serie</td><td><?= $this->nettoyer($voiture['serie']) ?></td></tr>
+    <tr><td>fabricant</td><td><?= $this->nettoyer($voiture['fabricant']) ?></td></tr>
+    <tr><td>couleur</td><td><?= $this->nettoyer($voiture['couleur']) ?></td></tr>
+    <tr><td>contenance</td><td><?= $this->nettoyer(voiture['contenance']) ?></td></tr>
+    <tr><td>chaises</td><td><?= $this->nettoyer($voiture['chaises']) ?></td></tr>
+    <tr><td>matricule</td><td><?= $this->nettoyer($voiture['matricule']) ?></td></tr>
+    <tr><td>vitesse_max</td><td><?= $this->nettoyer($voiture['vitesse_max']) ?></td></tr>
+    <tr><td>transmission</td><td><?= $this->nettoyer($voiture['transmission']) ?></td></tr>
+    <tr><td>transmission</td><td><?= $this->nettoyer($voiture['transmission']) ?></td></tr>
+    <tr><td>acceleration</td><td><?= $this->nettoyer($voiture['acceleration']) ?></td></tr>
+    <tr><td>puissance_moteur</td><td><?= $this->nettoyer($voiture['puissance_moteur']) ?></td></tr>
+    <tr><td>date de sortie</td><td><time><?= $this->nettoyer($voiture['jour'])." - ".$this->nettoyer($voiture['mois'])." - ".$this->nettoyer($voiture['annee']) ?></time></td></tr>
+</table>
+</p>
+    </article>
