e1bccc8a73e342c02b28f206461e1aa3536814c1
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 01:12:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
 
-require 'Controleur/Controleur.php';
+require 'Controleur/Routeur.php';
 
-try {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'voiture') {
-            if (isset($_GET['id'])) {
-                $id = intval($_GET['id']);
-                if ($id != 0) {
-                    voiture($id);
-                }
-                else
-                    throw new Exception("Identifiant non valide");
-            }
-            else
-                throw new Exception("Identifiant non défini");
-        }
-        else
-            throw new Exception("Action non valide");
-    }
-    else {  // aucune action définie : affichage de l'accueil
-        accueil();
-    }
-}
-catch (Exception $e) {
-    erreur($e->getMessage());
-}
+$routeur = new Routeur();
+$routeur->routerRequete();
