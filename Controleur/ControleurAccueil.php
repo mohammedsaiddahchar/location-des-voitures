53e3269a4d85d198a2641d1de0d96c4f1904b387
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 03:05:03 +0100

    Update ControleurAccueil.php

--- Controleur/ControleurAccueil.php
+++ Controleur/ControleurAccueil.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once 'Framework/Controleur.php';
 require_once 'Modele/Voiture.php';
-require_once 'Vue/Vue.php';
 
-class ControleurAccueil {
+class ControleurAccueil extends Controleur {
 
     private $voiture;
 
@@ -11,11 +11,10 @@ class ControleurAccueil {
         $this->voiture = new Voiture();
     }
 
-// Affiche la liste de tous les voitures
-    public function accueil() {
-        $voitures = $this->voiture->getVoitures();
-        $vue = new Vue("Accueil");
-        $vue->generer(array('voitures' => $voitures));
+    // Affiche la liste de tous les voitures
+    public function index() {
+        voitures = $this->voiture->getVoitures();
+        $this->genererVue(array('voitures' => voitures));
     }
 
 }
