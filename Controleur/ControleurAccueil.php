0c6faa798199dfac09610e92d3b95915d747d6eb
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 02:04:44 +0100

    Update and rename Controleur.php to ControleurAccueil.php

---
+++ Controleur/ControleurAccueil.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'Modele/Voiture.php';
+require_once 'Vue/Vue.php';
+
+class ControleurAccueil {
+
+    private $voiture;
+
+    public function __construct() {
+        $this->voiture = new Voiture();
+    }
+
+// Affiche la liste de tous les voitures
+    public function accueil() {
+        $voitures = $this->voiture->getVoitures();
+        $vue = new Vue("Accueil");
+        $vue->generer(array('voitures' => $voitures));
+    }
+
+}
