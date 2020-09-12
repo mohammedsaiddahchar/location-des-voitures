06c3f5cb4eeb807a6681b5c5b320d5f0c1951c94
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2020-09-12 03:10:10 +0100

    Update ControleurVoiture.php

--- Controleur/ControleurVoiture.php
+++ Controleur/ControleurVoiture.php
@@ -1,23 +1,29 @@
 <?php
 
+require_once 'Framework/Controleur.php';
 require_once 'Modele/Voiture.php';
-require_once 'Vue/Vue.php';
-
-class ControleurVoiture {
+/**
+ * Contrôleur des actions liées aux voitures
+ *
+ * @author Dahchar Mohammed Said
+ */
+class ControleurVoiture extends Controleur {
 
     private $voiture;
 
+    /**
+     * Constructeur 
+     */
     public function __construct() {
         $this->voiture = new Voiture();
     }
 
-    // Affiche les détails sur une voiture
-    public function voiture($id) {
+    // Affiche les détails sur un voiture
+    public function index() {
+        $id = $this->requete->getParametre("id");
+        
         $voiture = $this->voiture->getVoiture($id);
-        $vue = new Vue("Voiture");
-        $vue->generer(array('voiture' => $voiture));
+        
+        $this->genererVue(array('voiture' => $voiture));
     }
-
-   
-
 }
